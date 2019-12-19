04d41076948517acef74c618db4b8f5a3de9ff35
Shola Adeshina Hassan <delameh@icloud.com>
2019-12-20 00:06:00 +0100

    refined the logic

--- utils/constants.go
+++ utils/constants.go
@@ -6,8 +6,8 @@ const (
 	//HTTP Code
 	BadRequest        = http.StatusBadRequest
 
-	//Model
-	IdNotFound = " ID is Not Found!"
+	IdNotFound = "ID is Not Found!"
+    ImageNotFound = "Image is not Found"
 
 	// Image Path
 	ImagePath = "images"
